afeee7477f60faba4361cef460aaef30c62b6043
Bintr <20655963+explowz@users.noreply.github.com>
2023-08-15 19:45:25 +0300

    Fix TF2 not starting when using non-native Steam runtime

--- watchdog.sh
+++ watchdog.sh
@@ -8,6 +8,12 @@ bot_id=$3
 
 was_updating=false
 
+if [[ $steam_command == "steam-native" ]]; then
+  library_path="$LD_LIBRARY_PATH:./bin"
+else
+  library_path="$(/home/"$USER"/.local/share/Steam/ubuntu12_32/steam-runtime/run.sh printenv LD_LIBRARY_PATH):./bin"
+fi
+
 sleep 10
 
 while :; do
@@ -21,7 +27,7 @@ while :; do
 
   # Check if Steam is started and start TF2
   if [[ "$(pidof steam | wc -w)" -eq 1 && "$(pidof hl2_linux | wc -w)" -eq 0 ]]; then
-    cd /home/"$USER"/.local/share/Steam/steamapps/common/Team\ Fortress\ 2 && LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./bin" LD_PRELOAD="/opt/novisual.so:/opt/bin64/libvpcfs.so.0:/opt/bin32/libvpcfs.so.0" PULSE_SERVER="unix:/tmp/pulse.sock" ./hl2_linux -game tf -sw -small -w 640 -h 200 -novid -noasync -noassert -nosteamcontroller -nojoy -noshaderapi -nomouse -nocrashdialog -nomessagebox -nominidumps -nohltv -nobreakpad -nosrgb -nostartupsound -noquicktime -no_texture_stream -nouserclip -mat_softwaretl -precachefontchars -limitvsconst -particles 512 -snoforceformat -softparticlesdefaultoff -threads 1 +mat_aaquality 0 +mat_antialias 0 >/dev/null 2>&1 &
+    cd /home/"$USER"/.local/share/Steam/steamapps/common/Team\ Fortress\ 2 && LD_LIBRARY_PATH=$library_path LD_PRELOAD="/opt/novisual.so:/opt/bin64/libvpcfs.so.0:/opt/bin32/libvpcfs.so.0" PULSE_SERVER="unix:/tmp/pulse.sock" ./hl2_linux -game tf -sw -small -w 640 -h 200 -novid -noasync -noassert -nosteamcontroller -nojoy -noshaderapi -nomouse -nocrashdialog -nomessagebox -nominidumps -nohltv -nobreakpad -nosrgb -nostartupsound -noquicktime -no_texture_stream -nouserclip -mat_softwaretl -precachefontchars -limitvsconst -particles 512 -snoforceformat -softparticlesdefaultoff -threads 1 +mat_aaquality 0 +mat_antialias 0 >/dev/null 2>&1 &
     sleep 10 # Plenty of time for the TF2 process to be started
   fi
 
